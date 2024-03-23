42454711f4b61c95dc37ae3de855788818e97b8f
stefanhk31 <stefanhk31@users.noreply.github.com>
2024-03-23 18:50:50 +0000

    build:3b164908c913c04008c189188de507458b4cfd3c

--- artifact/web/flutter_service_worker.js
+++ artifact/web/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "04b727561b76af392beeb602f8a5266c",
-"/": "04b727561b76af392beeb602f8a5266c",
+const RESOURCES = {"index.html": "3e342c95ad1dc1c99c31121a5942ea52",
+"/": "3e342c95ad1dc1c99c31121a5942ea52",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
